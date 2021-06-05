7b565549aac6f7a60408552ac5c1bfc3acfbc5fc
Chen Junda <ddadaal@outlook.com>
2021-06-05 21:52:54 +0800

    add tests

--- tests/interpolation.test.tsx
+++ tests/interpolation.test.tsx
@@ -23,3 +23,31 @@ it("interpolates multiple text args", async () => {
     .toBe("inter arg1 po arg2 lation");
 });
 
+it("interpolates single ReactNode arg", async () => {
+  const wrapper = renderWithProvider(
+    <span data-testid="test">
+      <Localized id="interpolation1" args={[
+        <strong key="1">arg1</strong>,
+      ]}
+      />
+    </span>
+  );
+
+  expect(wrapper.getByTestId("test").innerHTML)
+    .toBe("inter <strong>arg1</strong> polation");
+});
+
+it("interpolates ReactNode and text args", async () => {
+  const wrapper = renderWithProvider(
+    <span data-testid="test">
+      <Localized id="interpolation2" args={[
+        <strong key="1">arg1</strong>,
+        "arg2",
+      ]}
+      />
+    </span>
+  );
+
+  expect(wrapper.getByTestId("test").innerHTML)
+    .toBe("inter <strong>arg1</strong> po arg2 lation");
+});
